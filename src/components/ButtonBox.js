4f1601f8a805921e77cde4b2bfafc5d304428d56
Sodeeqishola5 <91472029+Sodeeqishola5@users.noreply.github.com>
2022-10-08 15:00:28 +0100

    2 files modified

--- src/components/ButtonBox.js
+++ src/components/ButtonBox.js
@@ -1,8 +1,6 @@
-import React from 'react'
-const ButtonBox = ({children}) => {
-  return (
-      <div className="buttonBox">{children}</div>
-  )
-}
+import React from 'react';
+const ButtonBox = ({ children }) => {
+  return <div className="buttonBox">{children}</div>;
+};
 
-export default ButtonBox
+export default ButtonBox;
