69f3a07c7a4928264f4e335382310b57d839d5b4
Sodeeqishola5 <91472029+Sodeeqishola5@users.noreply.github.com>
2022-10-08 12:59:14 +0100

    1 file modified

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { useContext } from 'react'
-import { CalcContext } from './Context/CalcContext'
+import React from 'react';
+import { useContext } from 'react';
+import { CalcContext } from './Context/CalcContext';
 import { Textfit } from 'react-textfit';
 
 const Screen = () => {
-  const { calc } = useContext(CalcContext)
-  return <Textfit className="screen" max={70} mode="single">{calc.num ? calc.num : calc.res}</Textfit>
-}
+  const { calc } = useContext(CalcContext);
+  return (
+    <Textfit className="screen" max={70} mode="single">
+      {calc.num ? calc.num : calc.res}
+    </Textfit>
+  );
+};
 
-export default Screen
+export default Screen;
