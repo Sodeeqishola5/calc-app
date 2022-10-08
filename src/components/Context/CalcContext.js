50647376901bdb322d1b0c19ae348f9a6775416e
Sodeeqishola5 <91472029+Sodeeqishola5@users.noreply.github.com>
2022-10-08 13:26:18 +0100

    1 file modified

--- src/components/Context/CalcContext.js
+++ src/components/Context/CalcContext.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { createContext, useState } from "react"
+import React from 'react';
+import { createContext, useState } from 'react';
 
-export const CalcContext = createContext()
-const CalcProvider = ({children}) => {
-    const [calc, setCalc] = useState({
-        sign: "",
-        num: 0,
-        res: 0
-    });
+export const CalcContext = createContext();
+const CalcProvider = ({ children }) => {
+  const [calc, setCalc] = useState({
+    sign: '',
+    num: 0,
+    res: 0,
+  });
 
-    const providerValue = {
-        calc, setCalc
-    }
+  const providerValue = {
+    calc,
+    setCalc,
+  };
   return (
     <CalcContext.Provider value={providerValue}>
-        {children}
+      {children}
     </CalcContext.Provider>
-  )
-}
+  );
+};
 
-export default CalcProvider
-
+export default CalcProvider;
