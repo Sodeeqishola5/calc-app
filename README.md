32465355618f48b36929963d4ed38f746346f6f2
Sodeeqishola5 <91472029+Sodeeqishola5@users.noreply.github.com>
2022-10-08 10:06:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# calc-app
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-npeo4u)
---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import logo from './logo.svg'
+// import './App.css'
+import React from 'react'
+import './style.css'
+import CalcProvider from './components/Context/CalcContext'
+import Wrapper from './components/Wrapper'
+import Screen from './components/Screen'
+import ButtonBox from './components/ButtonBox'
+import Button from './components/Button'
+const btnValues = [
+  ['C', '+-', '%', '/'],
+  [7, 8, 9, 'x'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '='],
+]
+function App() {
+  return (
+    <CalcProvider>
+      <Wrapper>
+        <Screen />
+        <ButtonBox>
+          {btnValues.flat().map((btn, i) => (
+            <Button value={btn} key={i} />
+          ))}
+        </ButtonBox>
+      </Wrapper>
+    </CalcProvider>
+  )
+}
+
+export default App
+
